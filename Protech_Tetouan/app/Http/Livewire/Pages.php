c7151c10e1440adbfce0eb704ab63a4ad1edcf3a
Said <zitouni.sd@gmail.com>
2021-07-09 12:29:18 +0100

    create, update front to back added, delete front added

--- Protech_Tetouan/app/Http/Livewire/Pages.php
+++ Protech_Tetouan/app/Http/Livewire/Pages.php
@@ -1,17 +1,21 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
+use Livewire\WithPagination;
 
 use App\Models\Page;
-
+// used to validate slugs in our form validation 
+use Illuminate\Validation\Rule;
 class Pages extends Component
 {   
     public $modalFormVisible = false ;
+    public $modalFormDeleteVisible = false;
     public $slug;
     public $title;
     public $content;
+    public $modelId;
+    use WithPagination;
     
     
     /**
@@ -20,8 +24,138 @@ class Pages extends Component
      * @return void
      */
     public function createShowModal(){
+         $this->resetValidation();
+         $this->resetVars();
          $this->modalFormVisible = true;
     }    
+    
+    /**
+     * create function 
+     *
+     * @return void
+     */
+    public function create(){
+        $this->validate();
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }    
+    /**
+     * read function 
+     *
+     * @return void
+     */
+    public function read(){
+        return Page::paginate(5);
+    }
+    
+        /**
+     * rules for validation
+     *
+     * @return void
+     */
+
+    public function rules(){
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages','slug')],
+            'content' => 'required'
+        ];
+    }
+
+    /**
+     * modalData return the data the create function 
+     *
+     * @return void
+     */
+    public function modelData(){
+        $data= [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content
+        ];
+        return $data;
+    }    
+    /**
+     * resetVars reset variables of livewire
+     *
+     * @return void
+     */
+    public function resetVars(){
+        $this->modelId = null;
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+    
+    /**
+     * update the article
+     *
+     * @return void
+     */
+    public function update(){
+        // problem with slug when trying update
+        $this->validate();
+        Page::find($this->modelId)->update($this->modelData());
+        $this->modalFormVisible = false;
+    }
+    /**
+     * updatedTitle take the value of the title 
+     * and assign to the slug everytime it's changed
+     * @return void
+     */
+    public function updatedTitle($value){
+        $this->generateSlug($value);
+    }    
+    /**
+     * generateSlug a url slug
+     * based on the title
+     * @param  mixed $value
+     * @return void
+     */
+    public function generateSlug($value){
+        $process1 = str_replace(' ','-',$value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+    }
+    
+    /**
+     * updateShowModal shows the form modal 
+     *  in update mode
+     * @param  mixed $id
+     * @return void
+     */
+    public function updateShowModal($id){
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modelId = $id;
+        $this->modalFormVisible = true;
+        $this->loadModel();
+    }
+    
+    /**
+     * loadModel loads the data of the component
+     *  
+     * @return void
+     */
+    public function loadModel(){
+        $data = Page::find($this->modelId);
+        $this->title = $data->title;
+        $this->slug = $data->slug;
+        $this->content = $data->content;
+    }
+    
+    /**
+     * shows delete Modal
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function deleteShowModal($id){
+       $this->modelId = $id;
+       $this->modalFormDeleteVisible = true;
+    }
+
     /**
      * the livewire render function 
      *
@@ -29,6 +163,8 @@ class Pages extends Component
      */
     public function render()
     {
-        return view('livewire.pages');
+        return view('livewire.pages', [
+            'data' => $this->read(),
+        ]);
     }
 }
--- Protech_Tetouan/resources/views/livewire/pages.blade.php
+++ Protech_Tetouan/resources/views/livewire/pages.blade.php
@@ -5,25 +5,69 @@
         </x-jet-button>
     </div>
 
-    {{-- Modal Form --}}
+    {{-- data table --}}
+    <div class="flex flex-col">
+                    <table class='min-w-full divide-y divide-gray-200'>
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Link</th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody class="min-w-full divide-y divide-gray-200">
+                            @if ($data->count())
+                                @foreach ($data as $item )
+                                <tr>
+                                    <td class="px-6 py-3 text-sm whitespace-no-wrap">{{ $item->title }}</td>
+                                    <td class="px-6 py-3 text-sm whitespace-no-wrap">
+                                        <a class="text-indigo-600 hover:text-indigo-900"
+                                            href="{{ URL::to('/'.$item->slug) }}" 
+                                            target="_blank"> {{ $item->slug }}</a></td>
+                                    <td class="px-6 py-3 text-sm whitespace-no-wrap">{!! $item->content !!}</td>
+                                    <td class="px-6 py-3 text-sm whitespace-no-wrap">
+                                        <x-jet-button wire:click="updateShowModal({{ $item->id }})">
+                                            {{ __('Update') }}
+                                        </x-jet-button>
+                                        <x-jet-danger-button wire:click="deleteShowModal({{ $item->id }})">
+                                            {{ __('Delete') }}
+                                        </x-jet-button>
+                                    </td>
+                                </tr> 
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td class="px-6 py-3 text-sm whitespace-no-wrap" colspan="4">No results found</td>
+                                </tr>
+                            @endif
+                        </tbody>
+                    </table>
+    </div>
+   
+        {{ $data->links() }}
+    {{-- Modal Form for update and delete --}}
     <div class="mt-4">
         <x-jet-dialog-modal wire:model="modalFormVisible">
             <x-slot name="title">
-                {{ __('Save Page') }}
+                {{ __('Save Page') }} {{ $modelId }}
             </x-slot>
-        </div>
+    </div>
             <x-slot name="content">
                 <div class="mt-4">
                     <x-jet-label for="title" value="{{ __('Title') }}" />
                     <x-jet-input id="title" class="block mt-1 w-full" type="text" name="title" wire:model.debounce.800ms='title' />
-                </div>    
+                    @error('title') <span class="error"> {{ $message }}</span> @enderror
+                </div>  
+
                 <div class="mt-4">
-                    <x-jet-label for="title" value="{{ __('Slug') }}" /> 
+                    <x-jet-label for="slug" value="{{ __('Slug') }}" /> 
                     <span class="mt-1 items-center px-3 rounded-1-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
                         https://localhost:8000/
                     </span>
                     {{-- debounce avoid to many requests at the same time --}}
-                    <x-jet-input id="title" class="block mt-1 w-full rounded rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" wire:model.debounce.800ms='slug' type="text" name="slug" placeholder="url-slug" />
+                    <x-jet-input id="slug" class="block mt-1 w-full rounded rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" wire:model.debounce.800ms='slug' type="text" name="slug" placeholder="url-slug" />
+                    @error('slug') <span class="error"> {{ $message }}</span> @enderror
                 </div>  
                 <div class="mt-4">
                     <x-jet-label for="content" value="{{ __('Content') }}" />
@@ -40,18 +84,47 @@
                             </div>
                         </div>
                     </div>
+                    @error('content') <span class="error"> {{ $message }}</span> @enderror
                 </div>   
             </x-slot>
 
             <x-slot name="footer">
-                <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                <x-jet-secondary-button wire:click="$toggle('modalFormDeleteVisible')" wire:loading.attr="disabled">
                     {{ __('Nevermind') }}
                 </x-jet-secondary-button>
-
-                <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
-                    {{ __('Save') }}
-                </x-jet-button>
+                @if($modelId)
+                    <x-jet-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                        {{ __('Save') }}
+                    </x-jet-button>
+                @else
+                    <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                        {{ __('Create') }}
+                    </x-jet-button>
+                @endif
             </x-slot>
         </x-jet-dialog-modal>
+    
+    {{-- end model for create and update --}}
 
+    <!-- Delete User Confirmation Modal -->
+    <x-jet-dialog-modal wire:model="modalFormDeleteVisible">
+        <x-slot name="title">
+            {{ __('Delete Page') }}
+        </x-slot>
+
+        <x-slot name="content">
+            {{ __('Are you sure you want to delete your page? Once your page is deleted, all of its resources and data will be permanently deleted.') }}
+
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormDeleteVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="delete" wire:loading.attr="disabled">
+                {{ __('delete') }}
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
