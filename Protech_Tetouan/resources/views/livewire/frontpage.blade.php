08f3f0ea1d9b813398439abf956ac7890040e89f
Said <zitouni.sd@gmail.com>
2021-07-11 11:20:35 +0100

    display images from the storage

--- Protech_Tetouan/resources/views/livewire/frontpage.blade.php
+++ Protech_Tetouan/resources/views/livewire/frontpage.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="boder p-5 text-gray-500 bg-gray-700 text-4xl sm:bg-blue-400 md:text-5xl lg:bg-green-300">{{ $title }}</div>
-    <div class='border text-center lg:flex'> {!!  $content !!} </div>
+    <div class="p-5 text-4xl text-gray-500 bg-gray-700 boder sm:bg-blue-400 md:text-5xl lg:bg-green-300">{{ $title }}</div>
+    <div class='text-center border lg:flex'> {!!  $content !!} </div>
     <div class="border">
-        <img src="{{ asset('img/img01.jpg') }}" alt="">
+        <img src="{{ asset('storage/img/Youfoodshutterbug.jpg') }}" alt="">
     </div>
 </div>
