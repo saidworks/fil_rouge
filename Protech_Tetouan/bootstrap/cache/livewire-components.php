7740eeb5b651b6137e35033ae9bb976d19530477
Said <zitouni.sd@gmail.com>
2021-08-15 11:03:11 +0100

    fix branch

--- Protech_Tetouan/bootstrap/cache/livewire-components.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'counter' => 'App\\Http\\Livewire\\Counter',
-  'frontpage' => 'App\\Http\\Livewire\\Frontpage',
-  'navigation-menus' => 'App\\Http\\Livewire\\NavigationMenus',
-  'pages' => 'App\\Http\\Livewire\\Pages',
-  'products' => 'App\\Http\\Livewire\\Products',
-  'user-permissions' => 'App\\Http\\Livewire\\UserPermissions',
-  'users' => 'App\\Http\\Livewire\\Users',
-);
